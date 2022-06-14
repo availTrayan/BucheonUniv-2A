f745c7eb3d08f3f9e383f2083652780a93f38438
availTrayan <107421554+availTrayan@users.noreply.github.com>
2022-06-14 15:58:11 +0900

    Add files via upload

---
+++ test/TestMain.java
@@ -0,0 +1,18 @@
+package test;
+
+import week02.Cat;
+
+public class TestMain {
+
+	/**
+	 * @param args
+	 */
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Cat objCat = new Cat();
+
+	}
+
+}
